fe64bc56fe4e9e80377279d10f086b9cc32cf105
Esh <77237602+Someone-here@users.noreply.github.com>
2021-05-26 22:58:59 +0530

    Add files via upload

---
+++ Stocks.py
@@ -0,0 +1,47 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+crypto = True
+
+def stock(stock, exchange):
+    global crypto
+    if not crypto:
+        req = get(f"https://google.com/finance/quote/{stock}:{exchange}").text
+    else:
+        req = get(f"https://google.com/finance/quote/{stock}-{exchange}").text
+    soup = BeautifulSoup(req, "html.parser")
+    return float(soup.find("div", class_="fxKbKc").text.replace("$", "").replace(",", "").replace("₹", "").replace("€", ""))
+
+stk = input("Stock: ")
+exchange = input("Exchange: ")
+out = stock(stk, exchange)
+bought = True
+Buy_price = out
+going_up = True
+sell_price = out
+profit = out * -1
+print(f"current price: {out}")
+
+while True:
+    try:
+        if stock(stk, exchange) < out:
+            out = stock(stk, exchange)
+            if bought and out > Buy_price:
+                print("sell", out)
+                sell_price = out
+                profit = profit + sell_price
+                bought = False
+            else:
+                print(out, bought, profit)
+        elif stock(stk, exchange) > out:
+            out = stock(stk, exchange)
+            if not bought and out < sell_price:
+                print("buy", out)
+                Buy_price = out
+                profit = profit - Buy_price
+                bought = True
+            else:
+                print(out, bought, profit)
+    except:
+        print(profit)
+        
